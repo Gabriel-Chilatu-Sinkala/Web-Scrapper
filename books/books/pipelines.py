f608087cd148d215f8bad2d6cfbea5fc726032ce
Gabriel-Chilatu-Sinkala <120474088+Gabriel-Chilatu-Sinkala@users.noreply.github.com>
2025-05-10 05:06:32 +0200

    data processing and avoiding duplicate entries

--- books/books/pipelines.py
+++ books/books/pipelines.py
@@ -5,8 +5,11 @@
 
 
 # useful for handling different item types with a single interface
+
+import hashlib
 from itemadapter import ItemAdapter
 import pymongo
+from scrapy.exceptions import DropItem
 
 class BooksPipeline:
     def process_item(self, item, spider):
@@ -34,5 +37,17 @@ class MongoPipeline:
         self.client.close()
 
     def process_item(self, item, spider):
-        self.db[self.COLLECTION_NAME].insert_one(ItemAdapter(item).asdict())
+        item_id = self.compute_item_id(item)
+        item_dict = ItemAdapter(item).asdict()
+
+        self.db[self.COLLECTION_NAME].update_one(
+            filter={"_id": item_id},
+            update={"$set": item_dict},
+            upsert=True
+        )
+
         return item
+
+    def compute_item_id(self, item):
+        url = item["url"]
+        return hashlib.sha256(url.encode("utf-8")).hexdigest()
