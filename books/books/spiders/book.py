e523858971e80a2f642c13b5758c2822e3e581af
Gabriel-Chilatu-Sinkala <120474088+Gabriel-Chilatu-Sinkala@users.noreply.github.com>
2025-05-12 06:14:28 +0200

    testing

--- books/books/spiders/book.py
+++ books/books/spiders/book.py
@@ -2,6 +2,7 @@ import scrapy
 
 from books.items import BooksItem
 
+
 class BookSpider(scrapy.Spider):
     name = "book"
     allowed_domains = ["books.toscrape.com"]
@@ -12,7 +13,6 @@ class BookSpider(scrapy.Spider):
             yield scrapy.Request(
                 url, callback=self.parse, errback=self.log_error
             )
-
 
     def parse(self, response):
         """
@@ -27,13 +27,18 @@ class BookSpider(scrapy.Spider):
             item["title"] = book.css("h3 > a::attr(title)").get()
             item["price"] = book.css(".price_color::text").get()
             yield item
+
         next_page = response.css("li.next > a::attr(href)").get()
         if next_page:
             next_page_url = response.urljoin(next_page)
             self.logger.info(
                 f"Navigating to next page with URL {next_page_url}."
             )
-            yield scrapy.Request(url=next_page_url, callback=self.parse, errback=self.log_error,)
+            yield scrapy.Request(
+                url=next_page_url,
+                callback=self.parse,
+                errback=self.log_error,
+            )
 
     def log_error(self, failure):
         self.logger.error(repr(failure))
